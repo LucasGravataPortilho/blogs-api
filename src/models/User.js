687850bd1be8ef97de5a39aabefcf1a7aa38d411
Lucas Portilho <lucasportilho18@gmail.com>
2023-04-06 15:43:33 -0300

    feat. req 02

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+// src/models/User.js
+
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: DataTypes.INTEGER,
+      display_name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING
+    });
+  
+    return User;
+  };
+  
+  module.exports = UserModel;
