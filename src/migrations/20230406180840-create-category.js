d44b42d5831c5cb642eacbbb86402c200ec0dd6e
Lucas Portilho <lucasportilho18@gmail.com>
2023-04-06 15:09:56 -0300

    criando migration categories

---
+++ src/migrations/20230406180840-create-category.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey:true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('categories');
+  }
+};
