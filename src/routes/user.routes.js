1e2ebc7d4ee7194b5f525dfd53c580990d00982a
Lucas Portilho <lucasportilho18@gmail.com>
2023-04-06 18:51:31 -0300

    autenticação do token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const userController = require('../controllers/user.controller');
 const validateUser = require('../middlewares/validateUser');
+const validateJWT = require('../middlewares/validateJWT');
 
 router.route('/')
     .post(validateUser, userController.createUser);
