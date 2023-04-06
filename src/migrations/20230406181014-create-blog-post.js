ef62d5b359d9eec5856b1b0a19ff4cb4a6721242
Lucas Portilho <lucasportilho18@gmail.com>
2023-04-06 15:16:38 -0300

    criando migration post-blogs

---
+++ src/migrations/20230406181014-create-blog-post.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog-posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey:true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      published: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog-posts');
+  }
+};
